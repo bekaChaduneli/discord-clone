79731f67a6b16b6e2004749611c83f26f7a319a8
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-10-03 20:06:39 +0400

    server create modal finish

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
@@ -8,6 +9,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { NavigationAction } from "./navigation-action";
 import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -40,6 +42,17 @@ export const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
